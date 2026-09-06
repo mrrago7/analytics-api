312594b29cf3fb49799f0f6ce7cb3419138a326e
unknown <mar9aritastepanova@yandex.ru>
2026-09-06 19:30:36 +0300

    Added file apload

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from flask import request
+
+@app.route("/api/v1/files", methods=["POST"])
+def get_files():
+    file=request.files["file"]
+    return file
