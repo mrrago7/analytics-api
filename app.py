177c61cea6b786e267b3bd4a9c1194de15b1ec38
unknown <mar9aritastepanova@yandex.ru>
2026-09-08 23:33:45 +0300

    Add file upload validation and error handling

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask import request
+import data_processor
 
 @app.route("/api/v1/files", methods=["POST"])
 def get_files():
-    file=request.files["file"]
-    return file
+    try:
+        file=request.files["file"]
+    except KeyError:
+        return {"error": "File is required"}, 400
+    try:
+        data=data_processor.read_file(file)
+    except data_processor.UnsupportedFileType:
+        return {"error": "Unsupported file type"}, 415
+    json_data=data.to_json(orient='records', indent=2)
+    return json_data
--- data_processor.py
+++ data_processor.py
@@ -1,6 +1,15 @@
 import pandas as pd
+import os
 
+class UnsupportedFileType(Exception):
+    pass
 def read_file(file):
-    data=pd.read_csv(file)
+    file_name=file.filename
+    file_type=os.path.splitext(file_name)[1].lower()
+    if file_type=='.csv':
+        data=pd.read_csv(file)
+    elif file_type=='.xlsx':
+        data=pd.read_excel(file)
+    else:
+        raise UnsupportedFileType
     return data
-
